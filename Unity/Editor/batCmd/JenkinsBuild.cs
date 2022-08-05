28ad428e2dae9bdbbcd7f53911c0f92082e6cc70
445939112@qq.com <Xjq**github5059>
2022-08-05 10:49:17 +0800

    [ADD]batCmd/JenkinsBuild.cs

---
+++ Unity/Editor/batCmd/JenkinsBuild.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSBuildGame;
+
+public class JenkinsBuild
+{
+    private static PargmaTypeCheck mPargmaTypeCheck = new PargmaTypeCheck();
+    public static void Build() {
+        mPargmaTypeCheck.Init(Environment.GetCommandLineArgs());
+        bool buildRes = mPargmaTypeCheck.IsTrue(PargmaType.BRes);
+        bool IsDebug = mPargmaTypeCheck.IsTrue(PargmaType.IsDebug);
+        bool IsAAB = mPargmaTypeCheck.IsTrue(PargmaType.IsAAB);
+        bool IsMin = mPargmaTypeCheck.IsTrue(PargmaType.IsMin);
+        if (IsAAB) {
+            PlayerGenerator.Is_Build_Min_AAB = IsMin;
+        }
+        else {
+            PlayerGenerator.Is_Build_Min_APK = IsMin;
+        }
+        PlayerGenerator.ExportGoogleProject(IsDebug, buildRes, IsAAB, false);
+    }
+}
