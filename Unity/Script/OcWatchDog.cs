3de07d4cd9acc7d4b6f01410033351d73a95ce34
445939112@qq.com <Xjq**github5059>
2022-08-31 12:20:00 +0800

    [ADD]OcWatchDog

---
+++ Unity/Script/OcWatchDog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+public class OcWatchDog : IDisposable
+{
+    private string mDesc = "";
+    private DateTime mStartTime;
+    public static void RecordPorcessTime(string desc) {
+#if DEVELOPMENT_BUILD||UNITY_EDITOR || UNITY_ANDROID
+        Debug.Log($"<color=blue>### {desc} {DateTime.Now}</color>");
+#endif
+    }
+    public OcWatchDog(string desc)
+    {
+        this.mDesc = desc;
+        mStartTime = DateTime.Now;
+    }
+    public void Dispose()
+    {
+#if DEVELOPMENT_BUILD||UNITY_EDITOR  || UNITY_ANDROID
+        Debug.Log($"<color=green>### {mDesc}  StartTime: {mStartTime} EndTime:{DateTime.Now} UseTime:{(DateTime.Now - mStartTime).Seconds}</color>");
+#endif
+    }
+}
