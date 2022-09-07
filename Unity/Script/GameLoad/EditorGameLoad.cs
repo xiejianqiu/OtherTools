9aeda08af5f1b521e31d5599aa5f2f6b8578137f
445939112@qq.com <Xjq**github5059>
2022-09-07 20:22:08 +0800

    [ADD]GameLoad

---
+++ Unity/Script/GameLoad/EditorGameLoad.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+using System.IO;
+using System.Reflection;
+
+public class EditorGameLoad : GameLoadBase
+{
+    public override IEnumerator LoadAssembly()
+    {
+        yield return new WaitForEndOfFrame();
+        
+        FileStream fsdll = new FileStream(_hotfixdllPath,FileMode.Open);
+        //FileStream fspdb = new FileStream(_hotfixpdbPath,FileMode.Open);
+        
+        byte [] buffReaddll = new byte[fsdll.Length];
+        //byte [] buffReadpdb = new byte[fspdb.Length];
+        fsdll.Read(buffReaddll, 0, (int)fsdll.Length);
+        //fspdb.Read(buffReadpdb, 0, (int) fspdb.Length);
+        Assembly assembly = Assembly.Load(buffReaddll,null);
+        
+        Type type = assembly.GetType("HotFixStart");
+        object m_HotObj = assembly.CreateInstance("HotFixStart");
+        MethodInfo mStart = type.GetMethod("Start");
+        mStart.Invoke(m_HotObj,null);        
+    }
+}
---
+++ Unity/Script/GameLoad/GameLoadBase.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using ILRuntime.CLR.Method;
+using UnityEngine;
+
+public class GameLoadBase
+{
+    protected string _hotfixdllPath = Application.dataPath + "/BundleData/dll/hotfix.dll.bytes";
+    protected string _hotfixpdbPath = Application.dataPath + "/BundleData/dll/hotfix.pdb.bytes";
+    public static GameLoadBase CreateLoadLogic()
+    {
+        GameLoadBase vRet = null;
+#if UNITY_EDITOR && USE_HOTFIX
+        vRet = new HotFixEditorGameLoad();
+#elif UNITY_EDITOR
+        vRet = new EditorGameLoad();
+        //HotFixStart.Start();
+#elif  USE_HOTFIX
+        vRet = new iOSGameLoad();
+#else
+        vRet = new AndroidGameLoad();
+#endif        
+        return vRet;
+    }
+
+    public virtual IEnumerator LoadDll()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+
+    public virtual IEnumerator LoadAssembly()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+
+    public virtual void OnLoadAssmblyOk()
+    {
+#if !UNITY_EDITOR && !USE_HOTFIX
+        HotFixStart.Start();
+#endif       
+    }
+    public void InitILRuntime(ILRuntime.Runtime.Enviorment.AppDomain domain)
+    {
+        domain.RegisterCrossBindingAdaptor(new CoroutineAdapter());
+        domain.RegisterCrossBindingAdaptor(new ICompareAdapter());
+        domain.RegisterCrossBindingAdaptor(new ILRunTimeTypeICompareAdapter());
+        domain.RegisterValueTypeBinder(typeof(Vector3), new Vector3Binder());
+        domain.RegisterValueTypeBinder(typeof(Quaternion), new QuaternionBinder());
+        domain.RegisterValueTypeBinder(typeof(Vector2), new Vector2Binder());
+        AutoGeneralDelegate.Reg(domain);
+        DelegateConvertorGen.Reg(domain);
+        FunctionDelegateGen.Reg(domain);
+        ILHelper.InitProtoBuf(domain);
+    }
+    protected void LoadILRuntime(byte[] buffdll, byte[] buffpdb)
+    {
+        ILRuntime.Runtime.Enviorment.AppDomain appdomain = new ILRuntime.Runtime.Enviorment.AppDomain();
+        if (buffpdb == null)
+        {
+            appdomain.LoadAssembly(new MemoryStream(buffdll), null, null);
+        }
+        else
+        {
+            appdomain.LoadAssembly(new MemoryStream(buffdll), new MemoryStream(buffpdb), new ILRuntime.Mono.Cecil.Pdb.PdbReaderProvider());
+        }
+        InitILRuntime(appdomain);
+        appdomain.DebugService.StartDebugService(56000);
+        GfxLogicBrige.Instance.appDomain = appdomain;
+
+#if UNITY_EDITOR
+        appdomain.UnityMainThreadID = Thread.CurrentThread.ManagedThreadId;
+#endif
+
+#if USE_HOTFIX
+        ILRuntime.Runtime.Generated.CLRBindings.Initialize(appdomain);
+#endif
+        IMethod startMethod = appdomain.GetType("HotFixStart").GetMethod("Start", 0); ;
+        appdomain.Invoke(startMethod, null, null);
+    }
+}
