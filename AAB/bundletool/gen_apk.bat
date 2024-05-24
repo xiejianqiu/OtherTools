2d9b35aabcf782a6561ac619c794deb5012cac76
445939112@qq.com <Xjq**gb5059>
2024-05-24 11:07:05 +0800

    [M]gen_apk.bat

--- AAB/bundletool/gen_apk.bat
+++ AAB/bundletool/gen_apk.bat
@@ -1,7 +1,8 @@
-SET AAB_NAME=139_LY_VIETNAM_tg1_1.0.0.1_dtrue_vfalse_tfalse.aab
+SET AAB_NAME=tgsw1.aab
 SET APK_PATH=my_app.apks
 DEL %APK_PATH%
 REM java -jar bundletool-all-1.8.0.jar build-apks --bundle=%AAB_NAME% --output=%APK_PATH%
 java -jar bundletool-all-1.8.0.jar build-apks --bundle=%AAB_NAME% --output=%APK_PATH% --connected-device
+adb.exe connect 127.0.0.1 5555
 java -jar bundletool-all-1.8.0.jar install-apks --apks=%APK_PATH%
 PAUSE
