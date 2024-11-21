d6b93178a875278f4125deffdc9975f97aecfbeb
445939112@qq.com <Xjq**gb5059>
2024-11-21 11:02:58 +0800

    [M]添加加密工具

---
+++ Unity/GFrameWork/Plugins/Util/EncryUtil.cs
@@ -0,0 +1,25 @@
+﻿using Boo.Lang;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameFramework
+{
+    public class EncryUtil
+    {
+        static public void Encript(string filePath, string savePath)
+        {
+            var data = File.ReadAllBytes(filePath);
+            using (var myStream = new MyStream(savePath, FileMode.Create))
+            {
+                myStream.Write(data, 0, data.Length);
+            }
+        }
+        static public Stream Decript(string filePath)
+        {
+            var fileStream = new MyStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None, 1024 * 4, false);
+            return fileStream;
+        }
+    }
+}
---
+++ Unity/GFrameWork/Plugins/Util/MyStream.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+public class MyStream : FileStream
+{
+    const byte KEY = 40; // 密钥mask: 0010 1000
+    public MyStream(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize, bool useAsync) : base(path, mode, access, share, bufferSize, useAsync)
+    {
+
+    }
+    public MyStream(string path, FileMode mode) : base(path, mode)
+    {
+
+    }
+    // 重载读接口,一边读，一遍解密;
+    public override int Read(byte[] array, int offset, int count)
+    {
+        var index = base.Read(array, offset, count);
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] ^= KEY;
+        }
+        return index;
+
+    }
+    /// <summary>
+    /// 重载写接口，先加密再写入;
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="offset"></param>
+    /// <param name="count"></param>
+    public override void Write(byte[] array, int offset, int count)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] ^= KEY;
+        }
+        base.Write(array, offset, count);
+    }
+}
