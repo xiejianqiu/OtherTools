a822113bf4cd8177316a770d97326031c4958a20
445939112@qq.com <Xjq**gb5059>
2025-06-06 18:26:34 +0800

    [ADD]Encry

---
+++ Unity/Script/Encry/EncryUtil.cs
@@ -0,0 +1,51 @@
+﻿using Boo.Lang;
+using JetBrains.Annotations;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace GameFramework
+{
+    static public class EncryUtil
+    {
+        static public void EncryFile(string filePath, string savePath)
+        {
+            var data = File.ReadAllBytes(filePath);
+            using (var myStream = new MyStream(savePath, FileMode.Create))
+            {
+                myStream.Write(data, 0, data.Length);
+            }
+        }
+        static public Stream DecryFile(string filePath)
+        {
+            var fileStream = new MyStream(filePath, FileMode.Open, FileAccess.Read, FileShare.None, 1024 * 4, false);
+            return fileStream;
+        }
+        static public void DecryBytes(byte[] srcData)
+        {
+            for (int i = 0; i < srcData.Length; i++)
+            {
+                srcData[i] ^= MyStream.KEY;
+            }
+        }
+        /// <summary>
+        /// 加密目录中的ab资源
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="saveDir"></param>
+        static public void EncryDir(string srcDir, string saveDir)
+        {
+            if (Directory.Exists(saveDir))
+                Directory.Delete(saveDir, true);
+            Directory.CreateDirectory(saveDir);
+            var allFiles = Directory.GetFiles(srcDir, "*.ab", SearchOption.AllDirectories);
+            foreach (var file in allFiles)
+            {
+                var fileName = Path.GetFileName(file);
+                EncryUtil.EncryFile($"{srcDir}/{fileName}", $"{saveDir}/{fileName}");
+            }
+            Debug.LogError("混淆结束");
+        }
+    }
+}
---
+++ Unity/Script/Encry/MyStream.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+public class MyStream : FileStream
+{
+#if UNITY_ANDROID || U1GAME_QK
+    static public byte KEY = 66; // 密钥mask: 0010 1000
+#else
+    static public byte KEY = 67; // 密钥mask: 0010 1000
+#endif
+    public MyStream(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize, bool useAsync) : base(path, mode, access, share, bufferSize, useAsync)
+    {
+
+    }
+    public MyStream(string path, FileMode mode) : base(path, mode)
+    {
+
+    }
+    // 重载读接口,一边读，一遍解密;
+    public override int Read(byte[] array, int offset, int count)
+    {
+        var index = base.Read(array, offset, count);
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] ^= KEY;
+        }
+        return index;
+
+    }
+    /// <summary>
+    /// 重载写接口，先加密再写入;
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="offset"></param>
+    /// <param name="count"></param>
+    public override void Write(byte[] array, int offset, int count)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] ^= KEY;
+        }
+        base.Write(array, offset, count);
+    }
+}
