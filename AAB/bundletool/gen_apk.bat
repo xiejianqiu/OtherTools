42420a18212fcd1688ba05433ab10f9fce28e9e6
445939112@qq.com <Xjq**gb5059>
2025-08-08 18:17:20 +0800

    [M]gen_apk.bat

--- AAB/bundletool/gen_apk.bat
+++ AAB/bundletool/gen_apk.bat
@@ -1,8 +1,11 @@
-SET AAB_NAME=tgsw1.aab
+SET AAB_NAME=107_FRXX_KOREA_android_kr_1.0.0.1_1.0.7_7_dtrue_vtrue_efalse.aab
 SET APK_PATH=my_app.apks
 DEL %APK_PATH%
 REM java -jar bundletool-all-1.8.0.jar build-apks --bundle=%AAB_NAME% --output=%APK_PATH%
-java -jar bundletool-all-1.8.0.jar build-apks --bundle=%AAB_NAME% --output=%APK_PATH% --connected-device
-adb.exe connect 127.0.0.1 5555
-java -jar bundletool-all-1.8.0.jar install-apks --apks=%APK_PATH%
+REM adb connect 127.0.0.1:5555
+REM java -jar bundletool-all-1.8.0.jar build-apks --bundle=%AAB_NAME% --output=%APK_PATH% --connected-device
+java -jar bundletool-all-1.8.0.jar build-apks  --mode=universal --bundle=%AAB_NAME% --output=%APK_PATH%
+REM java -jar bundletool-all-1.8.0.jar install-apks --apks=%APK_PATH%
+REM java -jar bundletool-all-x.x.x.jar build-apks --bundle=/path/to/your/app.aab --output=/path/to/output.apks --mode=universal --ks=/path/to/your/keystore.jks --ks-pass=pass:your-keystore-password --ks-key-alias=your-key-alias --key-pass=pass:your-key-password
+
 PAUSE
